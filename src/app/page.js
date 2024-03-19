f37ef9102b5299baf651d3047668711d40ce6484
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-03-19 11:42:25 +0530

    Card content structure (besides star and links

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,21 @@ import { ExperienceCard } from "@/components/ExperienceCard/ExperienceCard";
 export default function Home() {
   return (
     <main>
-      <div>Hello GDSC</div>
-      <ExperienceCard />
+      <br/>
+      <ExperienceCard  name = "Robert Downey Jr." 
+                      ctcNum= "75 LpA" 
+                      positionData = "Lead"
+                      positionData2 = "Senior Lead"
+                      image = "https://source.unsplash.com/random/200x200"
+                      companyImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Goldman_Sachs.svg/2048px-Goldman_Sachs.svg.png"/>
+      <br/>
+      <ExperienceCard  name = "Robert Downey Jr." 
+                      ctcNum= "75 LpA" 
+                      positionData = "Lead"
+                      positionData2 = "Senior Lead"
+                      image = "https://source.unsplash.com/random/200x200"
+                      companyImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Goldman_Sachs.svg/2048px-Goldman_Sachs.svg.png"/>
+      
     </main>
   );
 }
--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -1,28 +1,50 @@
 
 
 import style from "./ExperienceCard.module.css";
+import React from 'react';
 
-import React from 'react'
-
-export const ExperienceCard = () => {
-  return (
-    <div className={style.ExperienceCard}>
-
-        <div className={style.box1}></div>
-        <div className={style.box11}></div>
-        <div className={style.box2}></div>
-        <div className={style.circle}></div>
-        <div className={style.circle2}>
-        </div>
-
-        <div className="name_company">
-            <div className="name">
-                <p>Name</p>
-                <h2>Robert Downy Junior</h2>
+export const ExperienceCard = ({ name, ctcNum, positionData, positionData2, image, companyImage }) => {
+    return (
+        <div className={style.ExperienceCard}>
+            <div className={style.box1}></div>
+            <div className={style.box11}>
+                <div className={style.name_company}>
+                    <div className={style.img_div}>
+                        <img src={image} alt="Avatar" className={style.img} />
+                    </div>
+                    <div className={style.name}>
+                        <p>Name</p>
+                        <h2>{name}</h2>
+                    </div>
+                    <div className={style.company}>
+                        <p>Company</p>
+                        <img src={companyImage} alt="" />
+                    </div>
+                    <div className={style.details}>
+                        <div className={style.ctc}>
+                            <p>CTC</p>
+                            <div className={style.ctc_box}>
+                                <p className={style.ctc_num}>{ctcNum}</p>
+                            </div>
+                        </div>
+                        <div className={style.position}>
+                            <p>Position</p>
+                            <div className={style.position_box}>
+                                <p className={style.position_num}>{positionData}</p>
+                            </div>
+                        </div>
+                        <div className={style.position2}>
+                            <p>Position</p>
+                            <div className={style.position_box2}>
+                                <p className={style.position_num2}>{positionData2}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
+            <div className={style.box2}></div>
+            <div className={style.circle}></div>
+            <div className={style.circle2}></div>
         </div>
-
-    </div>
-  )
-}
+    );
+};
