7f1777bdd67a0232eb538dea4c25b087465942e2
Mohamed zaheen <mohamedzaheen986@gmail.com>
2024-03-21 21:25:58 +0530

    Registration1 page and form1 created

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import About from "@/components/About/About";
+import Form1 from "@/components/Form1/Form1";
+import Registration from "@/components/Registration/Registration";
 
 export default function Home() {
   return (
     <div>
+      <Registration/>
+      <Form1/>
       <About/>
     </div>
   );
