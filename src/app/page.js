f16d97e43e3852b23ba18085de9f3528272465cf
Mohamed zaheen <mohamedzaheen986@gmail.com>
2024-03-25 00:15:12 +0530

    Get in touch page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import About from "@/components/About/About";
 import Form1 from "@/components/Form1/Form1";
+import GetInTouch from "@/components/GetInTouch/GetInTouch";
 import RegistrationHero from "@/components/RegistrationHero/RegistrationHero";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <RegistrationHero image={"Desk.svg"} tittle={"Insights that Matters"} />
       <Form1 />
       <About />
+      <GetInTouch/>
     </main>
   );
 }
