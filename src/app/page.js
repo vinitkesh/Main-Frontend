b52c6ed6e0fb8d4f384e5ca10b9f831bb63d3a07
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-03-26 16:46:28 +0530

    Card

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
-import Image from "next/image";
-
-import {Stars } from "@/components/Stars/Stars";
+import { ExpandedExperience } from "@/components/ExpandedExperience/ExpandedExperience";
 
 import { ExperienceCard } from "@/components/ExperienceCard/ExperienceCard";
 
 export default function Home() {
   return (
     <main>
+      <br/>
+
+      <ExpandedExperience name = "Robert Downey Jr." 
+                      stars={3}
+                      ctcNum= "75 LpA" 
+                      positionData = "Software Developer"
+                      positionData2 = "FTE"
+                      image = "https://source.unsplash.com/random/300x300"
+                      companyImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Goldman_Sachs.svg/2048px-Goldman_Sachs.svg.png"/>
+
       <br/>
 
       <ExperienceCard  name = "Robert Downey Jr." 
