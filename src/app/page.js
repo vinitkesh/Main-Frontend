8b07dbdc566e6f7b680ed95890d30032c689e7bf
EMANDI DEVI PRANEETH <praneethe4@gmail.com>
2024-07-03 12:36:44 +0530

    initial hero

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import Form1 from "@/components/Form1/Form1";
 import Form2 from "@/components/Form2/Form2";
 import GetInTouch from "@/components/GetInTouch/GetInTouch";
 import RegistrationHero from "@/components/RegistrationHero/RegistrationHero";
-
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main>
-
+      <HeroSection />
       <RegistrationHero image={"Desk.svg"} tittle={"Insights that Matters"} />
       <Form1 />
       <Form2 />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import Link from "next/link"
 import Image from "next/image"
 import NavUtils from "./NavUtils"
 
-
 export default function Navbar() {
     return (
         <div className="w-full bg-[#292929] pb-1">
